e3f9d9ead747982399e04b11c389119d3c35f220
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2022-12-19 09:18:33 -0300

    use input in custom form

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Control from "./src/components/Control";
+import Input from "./src/components/Input";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={{ width: "100%", padding: 16 }}>
+        <Control>
+          <Control.Label>Login</Control.Label>
+          <Input />
+        </Control>
+
+        <Control>
+          <Control.Label>Password</Control.Label>
+          <Input secureTextEntry={true} />
+        </Control>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
